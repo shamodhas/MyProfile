3fe70473a948d69e3995a46a13adb80881ce4263
shamodha-sahan-rathnamalala <shamodhasrathnamalala@gmail.com>
2023-06-26 02:30:18 +0530

    finalize

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 AOS.init();
+
 window.addEventListener("scroll",function (e){
     const squared = document.querySelector(".trapezoid");
     squared.classList.toggle("trapezoid-hide",window.scrollY > 150);
@@ -81,4 +82,65 @@ anime.timeline({ loop: true })
     delay: 0
 });
 
+// project carousel
+window.addEventListener('load', () => {
+    var
+        carousels = document.querySelectorAll('.my-project-carousel');
+    for (var i = 0; i < carousels.length; i++) carousel(carousels[i]);
+});
+function carousel(root) {
+    var
+        figure = root.querySelector('.project-carousel-item'),
+        nav = root.querySelector('.project-carousel-nav'),
+        images = figure.children,
+        n = images.length,
+        bfc = ('bfc' in root.dataset),
+        theta = 2 * Math.PI / n,
+        currImage = 0;
 
+    setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    window.addEventListener('resize', () => {
+        setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    });
+
+    setupNavigation();
+
+    function setupCarousel(n, s) {
+        var apothem = s / (2 * Math.tan(Math.PI / n));
+        figure.style.transformOrigin = `50% 50% ${-apothem}px`;
+
+        for (var i = 0; i < n; i++)
+            images[i].style.padding = `20px`;
+        for (i = 1; i < n; i++) {
+            images[i].style.transformOrigin = `50% 50% ${-apothem}px`;
+            images[i].style.transform = `rotateY(${i * theta}rad)`;
+        }
+        if (bfc)
+            for (i = 0; i < n; i++)
+                images[i].style.backfaceVisibility = 'hidden';
+
+        rotateCarousel(currImage);
+    }
+
+    function setupNavigation() {
+        nav.addEventListener('click', (e)=>{
+            e.stopPropagation();
+
+            var t = e.target;
+            if (t.tagName.toUpperCase() != 'BUTTON')
+                return;
+
+            if (t.classList.contains('next')) {
+                currImage++;
+            } else
+            {
+                currImage--;
+            }
+
+            rotateCarousel(currImage);
+        }, true);
+    }
+    function rotateCarousel(imageIndex) {
+        figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
+    }
+}
